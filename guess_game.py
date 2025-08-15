cd6cc345490dea6d31f2d504bcb8ff2482c7bf44
Dagim Tessema <50804190+dagimtessema@users.noreply.github.com>
2025-08-14 19:30:36 -0700

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,26 +1,16 @@
-import random  # Import the random module to generate a random number
-
-# Generate a random number between 1 and 100
-random_num = random.randint(1, 100)
-
-# Define the guessing game function
-def guess_game():
-    # Prompt the user to enter a guess
-    user_input = input("Please guess a number between 1 and 100: ")
-
-    # Check if the input is a digit
+# Check if the input is a digit
     if user_input.isdigit():
         guess = int(user_input)  # Convert input to an integer
 
-     # Initiate a while loop that runs until a break statement is executed   
-       while True:
+     # Initiate a while loop that runs until a break statement is executed
+        while True:
         # Compare the guess to the random number
             if guess == random_num: # Check if guess matches random number
                 print("🎉 Bingo!") # Congratulate user on correct guess
                 break # Exit the while loop and end the game as user has won
             # If guess is incorrect proceed to the next condition
             elif guess<random_num: # check if guess is lower than the random number
-                print("Wrong guess, try higher.") # Give hint 
+                print("Wrong guess, try higher.") # Give hint
                 user_input = input("Please guess a number between 1 and 100: ") # Prompt the user to enter a guess
                 if user_input.isdigit():
                     guess = int(user_input) # update guess with new value and go back to condition 1 of while loop
@@ -36,8 +26,9 @@ def guess_game():
                     print("Invalid input. Please enter a number.")
                     break
     else:
-        print("Invalid input. Please enter a number.") 
+        print("Invalid input. Please enter a number.")
         guess_game()  # Retry if input is not a valid number
 
 # Start the game
 guess_game()
+
