bbe0190cc876a9b8558a14feb944c382eeaa854b
Dagim Tessema <50804190+dagimtessema@users.noreply.github.com>
2025-08-14 19:02:30 -0700

    Update guess_game.py
    Improved code from a recursion to a while loop to prevent stack overflow.

--- guess_game.py
+++ guess_game.py
@@ -12,17 +12,31 @@ def guess_game():
     if user_input.isdigit():
         guess = int(user_input)  # Convert input to an integer
 
+     # Initiate a while loop that runs until a break statement is executed   
+       while True:
         # Compare the guess to the random number
-        if guess < random_num:
-            print("Wrong guess, try higher.") # Give hint to guess a higher number
-            guess_game()  # Recursively call the function to try again
-        elif guess>random_num:
-            print("Wrong guess, try lower.")  # Give hint to guess a lower number
-            guess_game()  # Recursively call the function to try again
-        else:
-            print("🎉 Bingo!")  # Correct guess
+            if guess == random_num: # Check if guess matches random number
+                print("🎉 Bingo!") # Congratulate user on correct guess
+                break # Exit the while loop and end the game as user has won
+            # If guess is incorrect proceed to the next condition
+            elif guess<random_num: # check if guess is lower than the random number
+                print("Wrong guess, try higher.") # Give hint 
+                user_input = input("Please guess a number between 1 and 100: ") # Prompt the user to enter a guess
+                if user_input.isdigit():
+                    guess = int(user_input) # update guess with new value and go back to condition 1 of while loop
+                else:
+                    print("Invalid input. Please enter a number.")
+                    break # if input is not a number exit the loop and end the game
+            elif guess>random_num: # Check if guess is higher than the random number
+                print("Wrong guess, try lower.")
+                user_input = input("Please guess a number between 1 and 100: ")
+                if user_input.isdigit():
+                    guess = int(user_input)
+                else:
+                    print("Invalid input. Please enter a number.")
+                    break
     else:
-        print("Invalid input. Please enter a number.")
+        print("Invalid input. Please enter a number.") 
         guess_game()  # Retry if input is not a valid number
 
 # Start the game
